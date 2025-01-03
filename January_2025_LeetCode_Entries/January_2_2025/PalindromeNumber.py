12f848d58c02c384a97d3c91bdf23b8c3cf163b8
Lara Priscilla Anderson <96963166+larapriscillaanderson@users.noreply.github.com>
2025-01-02 16:29:22 -0800

    added Palindrome Number python Solution file to January 2, 2025 LeetCode entry

---
+++ January_2025_LeetCode_Entries/January_2_2025/PalindromeNumber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # first check if x is negative, because negative numbers
+        # cannot be palindromes
+        if x < 0:
+            return False
+
+        # reverse the digits without converting the number to a string
+        if str(x) == str(x)[::-1]:
+            return True
+        return False
