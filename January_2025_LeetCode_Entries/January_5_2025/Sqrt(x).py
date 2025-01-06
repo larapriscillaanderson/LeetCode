35832fa3246c5cd464ce7d3632f4717fd1719d14
Lara Priscilla Anderson <96963166+larapriscillaanderson@users.noreply.github.com>
2025-01-05 18:13:50 -0800

    Update Sqrt(x).py
    updated the sqrt(x) python file to include soluton

--- January_2025_LeetCode_Entries/January_5_2025/Sqrt(x).py
+++ January_2025_LeetCode_Entries/January_5_2025/Sqrt(x).py
@@ -1,1 +1,30 @@
-placeholder
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # first check if x is non-zero
+        if x <= 1:
+            return x
+        left, right = 1, x
+
+        while left <= right:
+            # run the binary search only while the left is less than the right value
+            # continue this by using a midpoint calculation
+            mid = left + (right - left) // 2
+
+            if mid * mid == x:
+                # if the midpoint squared equals the desired x value, return the midpoint
+                return mid
+            elif mid * mid < x:
+                # if the midpoint squared is less than x,
+                # the square root lies to the right of mid, then left becomes mid + 1
+                left = mid + 1
+            else:
+                # if the midpoint squared is more than x,
+                # the square root lies to the left of mid, then right becomes mid - 1
+                right = mid - 1
+                # right will be the largest integer whose square is less 
+                # than or equal to x at the end of the loop
+        return right
