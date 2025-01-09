b054b3ab39ea7fc2f72c6d41d38d88eaafa55f55
Lara Priscilla Anderson <96963166+larapriscillaanderson@users.noreply.github.com>
2025-01-08 19:18:20 -0800

    updated January 7, 2025 LeetCode Entry to include solution file

---
+++ January_2025_LeetCode_Entries/January_7_2025/LongestCommonPrefix.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            # if the input array is empty, return an empty string
+            return ""
+        
+        # initialize the prefix value to the first string in the array
+        prefix = strs[0]
+        
+        # iterate through the strings in the array starting from the second one
+        for current_string in strs[1:]:
+            # find the length of the common prefix 
+            # between prefix and the current string
+            common_index = 0
+            while common_index < len(prefix) and common_index < len(current_string) and prefix[common_index] == current_string[common_index]:
+                common_index += 1
+            
+            # update the prefix to the common part
+            prefix = prefix[:common_index]
+
+            # if the prefix becomes empty, return an empty string
+            if not prefix:
+                return ""
+
+        return prefix
